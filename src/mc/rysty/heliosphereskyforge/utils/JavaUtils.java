f43dc763eb267a5e65ff2f1bcb692dd1e512eef2
Ryan Styron <ryanstyron1220@gmail.com>
2020-07-29 20:45:53 -0400

    Add getRomanNumeral(int) function to JavaUtils.java.

--- src/mc/rysty/heliosphereskyforge/utils/JavaUtils.java
+++ src/mc/rysty/heliosphereskyforge/utils/JavaUtils.java
@@ -10,4 +10,35 @@ public class JavaUtils {
                 return entry.getKey();
         return null;
     }
+
+    /*
+     * This method only converts integers one through ten to Roman Numerals,
+     * otherwise it will return the number in base ten.
+     */
+    public static String getRomanNumeral(int baseTenNumber) {
+        String romanNumeral = "" + baseTenNumber;
+
+        if (baseTenNumber == 1)
+            romanNumeral = "I";
+        else if (baseTenNumber == 2)
+            romanNumeral = "II";
+        else if (baseTenNumber == 3)
+            romanNumeral = "III";
+        else if (baseTenNumber == 4)
+            romanNumeral = "IV";
+        else if (baseTenNumber == 5)
+            romanNumeral = "V";
+        else if (baseTenNumber == 6)
+            romanNumeral = "VI";
+        else if (baseTenNumber == 7)
+            romanNumeral = "VII";
+        else if (baseTenNumber == 8)
+            romanNumeral = "VIII";
+        else if (baseTenNumber == 9)
+            romanNumeral = "IX";
+        else if (baseTenNumber == 10)
+            romanNumeral = "X";
+
+        return romanNumeral;
+    }
 }
